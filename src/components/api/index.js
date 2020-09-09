f2b3b94bb0f429cf14d8b26d4830c2015d651e80
ysherqawi <ysherqawi@gmail.com>
2020-09-09 17:46:11 +0300

    added fetchCountries function

--- src/components/api/index.js
+++ src/components/api/index.js
@@ -1,14 +1,19 @@
 import axios from 'axios';
 const url = 'https://covid19.mathdro.id/api';
 
-export const fetchData = async () => {
+export const fetchData = async (country) => {
+  let changableUrl = url;
+
+  if (country) changableUrl = `${url}/countries/${country}`;
   try {
     const {
       data: { confirmed, recovered, deaths, lastUpdate },
-    } = await axios.get(url);
+    } = await axios.get(changableUrl);
 
     return { confirmed, recovered, deaths, lastUpdate };
-  } catch (error) {}
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const fetchDailyData = async () => {
@@ -22,5 +27,19 @@ export const fetchDailyData = async () => {
     }));
 
     return modifiedData;
-  } catch (error) {}
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const fetchCountries = async () => {
+  try {
+    const {
+      data: { countries },
+    } = await axios.get(`${url}/countries`);
+
+    return countries.map((country) => country.name);
+  } catch (error) {
+    console.log(error);
+  }
 };
