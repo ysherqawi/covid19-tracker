ffe00d372a5f1a92cce0d1fe7c7a8f80260b40db
ysherqawi <ysherqawi@gmail.com>
2020-09-09 18:25:01 +0300

    added and styled covid image

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { Cards, Chart, CountryPicker } from './components/';
-import styles from './App.module.css';
 import { fetchData } from './components/api';
+
+import covidImg from './images/image.png';
+import styles from './App.module.css';
 
 class App extends Component {
   state = {
@@ -24,6 +26,7 @@ class App extends Component {
     const { data, country } = this.state;
     return (
       <div className={styles.container}>
+        <img src={covidImg} alt='Covid-19' className={styles.image} />
         <Cards data={data} />
         <CountryPicker handleCountyChange={this.handleCountyChange} />
         <Chart data={data} country={country} />
