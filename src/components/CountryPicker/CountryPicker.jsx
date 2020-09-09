0d05d9ad2bb90e3e7a74592d30742b19e8d7d8a2
ysherqawi <ysherqawi@gmail.com>
2020-09-09 17:47:10 +0300

    created countrypicker component

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { NativeSelect, FormControl } from '@material-ui/core';
 
-const CountryPicker = () => {
+import styles from './CountryPicker.module.css';
+import { fetchCountries } from './../api/index';
+
+const CountryPicker = ({ handleCountyChange }) => {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const fetchAPI = async () => {
+      setCountries(await fetchCountries());
+    };
+
+    fetchAPI();
+  }, [setCountries]);
   return (
     <div>
-      <h2>CountryPicker</h2>
+      <FormControl className={styles.formControl}>
+        <NativeSelect
+          defaultValue=''
+          onChange={(e) => handleCountyChange(e.target.value)}
+        >
+          <option value=''>Global</option>
+          {countries.map((country, i) => (
+            <option key={i} value={country}>
+              {country}
+            </option>
+          ))}
+        </NativeSelect>
+      </FormControl>
     </div>
   );
 };
